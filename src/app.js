4226accba615a8a9b90914a03d4f393a1dcb2675
Xuelin <xuelin@mit.edu>
2019-01-18 14:15:51 -0500

    hi

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const views = require('./routes/views');
 
 const app = express();
 
+require('dotenv').config();
+
 app.use('/', views);
 app.use('/static', express.static('public'));
 
@@ -21,7 +23,7 @@ app.use(function (err, req, res, next) {
     })
 });
 
-const port = 3000;
+const port = (process.env.PORT || 3000);
 server = http.Server(app);
 server.listen(port, function () {
     console.log('server listening on port ' + port);
